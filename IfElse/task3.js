3a33eef1bc17aed926d9a6b98c89d338e89535a5
elmira.nalbandyan1 <elmira.nalbandyan@picsart.com>
2024-08-17 15:50:52 +0400

    if else statements

---
+++ IfElse/task3.js
@@ -0,0 +1,5 @@
+let result;
+let a = 5;
+let b = 3;
+
+result = (a + b + 4 < 0) ? 'Below' : 'Over';
---
+++ IfElse/task4.js
@@ -0,0 +1,4 @@
+let message;
+let login = 'Employee';
+
+message = login === 'Employee' ? 'Hello' : login === 'Director' ? 'Greetings' : login === '' ? 'No login' : '';
